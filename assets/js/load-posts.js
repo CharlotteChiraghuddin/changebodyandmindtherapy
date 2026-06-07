3f475bcf88b4559551d463c8830ecda51d45a1b8
[Charlotte Chiraghuddin] <[charlottechiraghuddinxo@gmail.com]>
2026-06-07 09:42:11 +0100

    Updated load-posts.js fetch request to point to the correct endpoint

--- assets/js/load-posts.js
+++ assets/js/load-posts.js
@@ -2,7 +2,7 @@ async function loadPosts() {
   const postsList = document.getElementById("reflections-list");
 
   // Fetch the list of posts from the GitHub API
-  const response = await fetch("https://api.github.com/repos/charlottetechiraghuddin/sarah_reeve/contents/posts");
+  const response = await fetch("https://api.github.com/repos/charlottetechiraghuddin/changebodyandmindtherapy/contents/posts");
   const files = await response.json();
 
   // Loop through each markdown file
